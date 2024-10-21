d7df57793d0a9402c572cd22fa05dccf9741e67c
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-21 22:33:40 +0300

    Добавить считывание команды из консоли

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <conversion-type> <input-file> <output-file>" << std::endl;
+        return 1;
+    }
+
+    std::string conversionType = argv[1];
+    std::string inputFileName = argv[2];
+    std::string outputFileName = argv[3];
+
+    if (conversionType == "mealy-to-moore") {
+
+    } else if (conversionType == "moore-to-mealy") {
+
+    } else {
+        std::cerr << "Unknown conversion type: " << conversionType << std::endl;
+        return 1;
+    }
+
     return 0;
 }
