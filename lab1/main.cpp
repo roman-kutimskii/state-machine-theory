aa103c643bbec0ae84b0930c75c2ba0869f0ef3f
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-22 21:07:15 +0300

    Добавить считывание автомата Мили

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,5 +1,46 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+struct MealyState {
+    std::unordered_map<std::string, std::pair<std::string, std::string> > transitions;
+};
+
+std::unordered_map<std::string, MealyState> readMealyMachine(const std::string &fileName) {
+    std::ifstream file(fileName);
+    std::string line;
+    std::unordered_map<std::string, MealyState> mealy;
+    std::vector<std::string> states;
+
+    getline(file, line);
+    std::stringstream ss(line);
+    std::string state;
+    getline(ss, state, ';');
+
+    while (getline(ss, state, ';')) {
+        states.push_back(state);
+    }
+
+    while (getline(file, line)) {
+        std::stringstream ssLine(line);
+        std::string input, transition;
+        getline(ssLine, input, ';');
+
+        for (const auto &stateName: states) {
+            getline(ssLine, transition, ';');
+            size_t slashPos = transition.find('/');
+            std::string nextState = transition.substr(0, slashPos);
+            std::string output = transition.substr(slashPos + 1, transition.size());
+            mealy[stateName].transitions[input] = {nextState, output};
+        }
+    }
+
+    return mealy;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -12,9 +53,8 @@ int main(int argc, char *argv[]) {
     std::string outputFileName = argv[3];
 
     if (conversionType == "mealy-to-moore") {
-
+        auto mealy = readMealyMachine(inputFileName);
     } else if (conversionType == "moore-to-mealy") {
-
     } else {
         std::cerr << "Unknown conversion type: " << conversionType << std::endl;
         return 1;
