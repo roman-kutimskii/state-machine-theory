ad259ba57ebd13b6e5d5cac3532fc8f09d7eeeb2
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-12-01 11:13:13 +0300

    Добавить базу

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <map>
+
+#include "state-machine-utils.h"
+
+std::unordered_map<std::string, MealyState> minimizeMealy(const std::unordered_map<std::string, MealyState> &mealy) {
+    auto minimized = mealy;
+    removeUnreachableStatesMealy(minimized);
+
+    return minimized;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const auto mealy = readMealyMachine("mealy.csv");
+    const auto minimized = minimizeMealy(mealy);
+    writeMealyMachine(minimized, "output.csv");
     return 0;
 }
