cc8b0dc81d21894d8184e6da14387b2c2c83ed29
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-12-08 21:52:44 +0300

    Добавить функцию для заполнения эпсилон

--- lab4/main.py
+++ lab4/main.py
@@ -6,11 +6,11 @@ def read_machine_from_file(file_path):
     finite_state = None
     states = []
     machine = {}
-    with open(file_path, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
+    with open(file_path, "r", newline="", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
         finite_markers = next(reader)[1:]
         states = next(reader)[1:]
-        finite_state = states[finite_markers.index('F')]
+        finite_state = states[finite_markers.index("F")]
         for row in reader:
             symbol = row[0]
             for i in range(1, len(row)):
@@ -20,15 +20,37 @@ def read_machine_from_file(file_path):
                         "is_finite": state == finite_state,
                         "transitions": {}
                     }
-                if symbol not in machine[state]["transitions"]:
-                    machine[state]["transitions"][symbol] = []
-                machine.get(state)["transitions"].get(symbol).append(row[i])
+                machine[state]["transitions"][symbol] = row[i].split(",")
 
     return states[0], machine
 
 
+def fill_epsilon(machine):
+    epsilon = {}
+
+    for state in machine:
+        transitions = machine[state]["transitions"]["ε"]
+        visited = set()
+        stack = [state]
+
+        while stack:
+            vertex = stack.pop()
+
+            if vertex not in visited:
+                visited.add(vertex)
+
+                for neighbor in machine[vertex]["transitions"]["ε"]:
+                    if neighbor:
+                        stack.append(neighbor)
+
+        epsilon[state] = list(visited)
+
+    return epsilon
+
+
 def process_machine(input_file_name, output_file_name):
     initial_state, machine = read_machine_from_file(input_file_name)
+    epsilon = fill_epsilon(machine)
 
 
 def main():
@@ -48,5 +70,5 @@ def main():
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
