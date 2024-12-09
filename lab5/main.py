818aadc642ff3cf1aec4b8a565aadd6ad5d4ff05
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-12-09 11:27:12 +0300

    Добавить набросок программы

--- lab5/main.py
+++ lab5/main.py
@@ -1,7 +1,24 @@
 import sys
 
 
+def process_regex(regex_pattern, output_file_name):
+    pass
+
+
 def main():
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <output-file> <regex pattern>")
+        return 1
+
+    output_file_name = sys.argv[1]
+    regex_pattern = sys.argv[2]
+
+    try:
+        process_regex(regex_pattern, output_file_name)
+    except RuntimeError as e:
+        print(e)
+        return 1
+
     return 0
 
 
