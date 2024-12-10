c1a1c62e8cd1998470bb8cd7fd17056be5595e0d
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-12-10 19:03:02 +0300

    Добавить вывод автомата

--- lab5/main.py
+++ lab5/main.py
@@ -1,8 +1,59 @@
+import csv
 import sys
 
 
+def parse_regex(regex):
+    machine = {
+        'ss': {},
+        'sf': {}
+    }
+
+    literal = regex[0]
+    machine['s0'] = {}
+    machine['ss'][literal] = 's0'
+
+    literal = regex[1]
+    machine['s1'] = {}
+    machine['s0'][literal] = 's1'
+
+    literal = regex[2]
+    machine['s1'][literal] = 'sf'
+
+
+
+    return machine
+
+
+def write_machine(machine, output_file_name):
+    symbols = set()
+    for state in machine:
+        transitions = machine[state]
+        for symbol in transitions:
+            symbols.add(symbol)
+
+    finite_markers = [""]
+    for state in machine:
+        finite_markers.append("F" if state == "sf" else "")
+
+    states = [""] + [state for state in machine]
+
+    with open(output_file_name, 'w', newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, delimiter=";")
+        writer.writerow(finite_markers)
+        writer.writerow(states)
+        for symbol in symbols:
+            row = [symbol]
+            for state in states[1:]:
+                transition = ""
+                if symbol in machine[state]:
+                    transition = machine[state][symbol]
+                row.append(transition)
+            writer.writerow(row)
+
+
 def process_regex(regex_pattern, output_file_name):
-    pass
+    machine = parse_regex(regex_pattern)
+    write_machine(machine, output_file_name)
 
 
 def main():
