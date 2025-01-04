116a11bf9ebb5ae09e47d8fda67e155f3828722a
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-04 10:45:18 +0300

    Добавить минимизацию КА Мура

--- lab2-python/main.py
+++ lab2-python/main.py
@@ -115,7 +115,53 @@ def minimize_mealy_machine(states, input_symbols, transitions, initial_state):
 
 
 def minimize_moore_machine(states, input_symbols, transitions, outputs, initial_state):
-    return states, input_symbols, transitions, outputs, initial_state
+    output_groups = {}
+    for state in states:
+        output = outputs[state]
+        output_groups.setdefault(output, set()).add(state)
+
+    partitions = list(output_groups.values())
+
+    def refine(partitions):
+        new_partitions = []
+        for group in partitions:
+            subgroup = {}
+            for state in group:
+                key = tuple(outputs[transitions[state][symbol]] for symbol in input_symbols)
+                subgroup.setdefault(key, set()).add(state)
+            new_partitions.extend(subgroup.values())
+        return new_partitions
+
+    while True:
+        new_partitions = refine(partitions)
+        if new_partitions == partitions:
+            break
+        partitions = new_partitions
+
+    state_map = {}
+    minimized_states = []
+    minimized_transitions = {}
+    minimized_outputs = {}
+    minimized_initial_state = None
+
+    for i, group in enumerate(partitions):
+        new_state = f"S{i}"
+        for state in group:
+            state_map[state] = new_state
+        minimized_states.append(new_state)
+        representative = next(iter(group))
+        minimized_outputs[new_state] = outputs[representative]
+
+    for group in partitions:
+        representative = next(iter(group))
+        new_state = state_map[representative]
+        minimized_transitions[new_state] = {
+            symbol: state_map[transitions[representative][symbol]]
+            for symbol in input_symbols
+        }
+    minimized_initial_state = state_map[initial_state]
+
+    return minimized_states, input_symbols, minimized_transitions, minimized_outputs, minimized_initial_state
 
 
 def main():
