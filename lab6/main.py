406b0f9433704061233da0e74ffe1c5d2bf5d77f
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-06 22:34:25 +0300

    Заполнить список токенов

--- lab6/main.py
+++ lab6/main.py
@@ -5,17 +5,17 @@ from lexer import Lexer
 
 
 def process(input_file_name, output_file_name):
-    with open(input_file_name, "r") as input_file:
+    with open(input_file_name, 'r') as input_file:
         text = input_file.readlines()
     lexer = Lexer(text)
     tokens = lexer.tokenize()
-    with open(output_file_name, "w") as output_file:
+    with open(output_file_name, 'w') as output_file:
         output_file.write(json.dumps(tokens, indent=4))
 
 
 def main():
     if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <input-file> <output-file>")
+        print(f'Usage: {sys.argv[0]} <input-file> <output-file>')
         return 1
 
     input_file_name = sys.argv[1]
@@ -30,5 +30,5 @@ def main():
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
--- lab6/token_type.py
+++ lab6/token_type.py
@@ -1,3 +1,6 @@
+from constants import *
+
+
 class TokenType:
     def __init__(self, name: str, regex: str):
         self.name = name
@@ -5,14 +8,43 @@ class TokenType:
 
 
 token_types_list = {
-    'NUMBER': TokenType('NUMBER', '[0-9]*'),
-    'VARIABLE': TokenType('VARIABLE', '[а-я]*'),
+    'ARRAY': TokenType('ARRAY', '(A|a)(R|r)(R|r)(A|a)(Y|y)'),
+    'BEGIN': TokenType('BEGIN', 'BEGIN'),
+    'ELSE': TokenType('ELSE', 'ELSE'),
+    'END': TokenType('END', 'END'),
+    'IF': TokenType('IF', 'IF'),
+    'OF': TokenType('OF', 'OF'),
+    'OR': TokenType('OR', 'OR'),
+    'PROGRAM': TokenType('PROGRAM', 'PROGRAM'),
+    'PROCEDURE': TokenType('PROCEDURE', 'PROCEDURE'),
+    'THEN': TokenType('THEN', 'THEN'),
+    'TYPE': TokenType('TYPE', 'TYPE'),
+    'VAR': TokenType('VAR', 'VAR'),
+    'MULTIPLICATION': TokenType('MULTIPLICATION', '\*'),
+    'PLUS': TokenType('PLUS', '\+'),
+    'MINUS': TokenType('MINUS', '-'),
+    'DIVIDE': TokenType('DIVIDE', '/'),
     'SEMICOLON': TokenType('SEMICOLON', ';'),
-    'SPACE': TokenType('SPACE', '[ \\n\\t\\r]'),
-    'ASSIGN': TokenType('ASSIGN', 'РАВНО'),
-    'LOG': TokenType('LOG', 'КОНСОЛЬ'),
-    'PLUS': TokenType('PLUS', 'ПЛЮС'),
-    'MINUS': TokenType('MINUS', 'МИНУС'),
-    'LPAR': TokenType('LPAR', '\\('),
-    'RPAR': TokenType('RPAR', '\\)'),
+    'COMMA': TokenType('COMMA', ','),
+    'LEFT_PAREN': TokenType('LEFT_PAREN', '\('),
+    'RIGHT_PAREN': TokenType('RIGHT_PAREN', '\)'),
+    'LEFT_BRACKET': TokenType('LEFT_BRACKET', '['),
+    'RIGHT_BRACKET': TokenType('RIGHT_BRACKET', ']'),
+    'EQ': TokenType('EQ', '='),
+    'GREATER': TokenType('GREATER', '>'),
+    'LESS': TokenType('LESS', '<'),
+    'LESS_EQ': TokenType('LESS_EQ', '<='),
+    'GREATER_EQ': TokenType('GREATER_EQ', '>='),
+    'NOT_EQ': TokenType('NOT_EQ', '<>'),
+    'COLON': TokenType('COLON', ':'),
+    'ASSIGN': TokenType('ASSIGN', ':='),
+    'DOT': TokenType('DOT', '.'),
+    'IDENTIFIER': TokenType('IDENTIFIER', f'{LETTER}|_({SYMBOL}|_)*'),
+    'STRING': TokenType('STRING', f'\'{SYMBOL}*\''),
+    'INTEGER': TokenType('INTEGER', f'(ε|-){NUMBER}'),
+    'FLOAT': TokenType('FLOAT', f'(ε|-){NUMBER}.{DIGIT}+'),
+    'LINE_COMMENT': TokenType('LINE_COMMENT', f'//{SYMBOL}*\n'),
+    'BLOCK_COMMENT': TokenType('BLOCK_COMMENT', f'{{{SYMBOL}*}}'),
+    'BAD': TokenType('BAD', f'{SYMBOL}*'),
+    'SPACE': TokenType('SPACE', f' |\\n|\\t|\\r'),
 }
