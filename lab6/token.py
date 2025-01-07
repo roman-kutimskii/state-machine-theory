bfa905d97fac66f3acaeac5b64e479bb84b03e19
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-07 10:30:41 +0300

    Добавить Token

---
+++ lab6/token.py
@@ -0,0 +1,11 @@
+from token_type import TokenType
+
+
+class Token:
+    def __init__(self, type: TokenType, value: str, pos: (int, int)):
+        self.type = type
+        self.value = value
+        self.pos = pos
+
+    def __repr__(self):
+        return f'{self.type} ({self.pos}) "{self.value}"'
