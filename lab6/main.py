ea3ef9fef11430c4e985eef45df8d872c1c902ba
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-09 16:44:15 +0300

    Улучшить взаимодействие с пользователем

--- lab6/main.py
+++ lab6/main.py
@@ -4,12 +4,13 @@ from lexer import Lexer
 
 
 def main() -> int:
-    if len(sys.argv) != 3:
-        print(f'Usage: python {sys.argv[0]} <input-file> <output-file>')
+    if len(sys.argv) not in {3, 4}:
+        print(f'Usage: python {sys.argv[0]} <input-file> <output-file> [debug]')
         return 1
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    debug = sys.argv[3] == 'debug' if len(sys.argv) == 4 else False
 
     lexer = Lexer(input_file)
 
@@ -18,8 +19,9 @@ def main() -> int:
             token = lexer.next_token()
             if token is None:
                 break
-            print(token)
-            output.write(str(token) + '\n')
+            if token.type != 'SPACE':
+                print(token) if debug else None
+                output.write(str(token) + '\n')
 
     lexer.close()
 
